7e9b5b3b99671277d5f9a0908df72802a6fc912b
amna <amna@gmail.com>
2023-07-26 10:26:46 +0500

    my first commit

---
+++ js/dashboard.js
@@ -0,0 +1,17 @@
+function toggleSidebar() {
+  var sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('sidebar-expanded');
+}
+
+
+// function openSidebar() {
+//   document.querySelector(".sidebar").style.left = "0";
+//   document.querySelector(".content").style.marginLeft = "100px";
+// }
+
+// function closeSidebar() {
+
+//   document.querySelector(".sidebar").style.left = "-300px";
+//   document.querySelector(".content").style.marginLeft = "0";
+
+// }
